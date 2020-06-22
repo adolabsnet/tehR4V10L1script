3876493a151b48534bc8ff3ad833c5aea0b20dbe
ravi0li <mikel.veg777@gmail.com>
2020-06-22 02:38:42 +0200

    Delete channel_test1.sh

--- tehR4V10L1script/003-Starting_with_nmap/channel_test1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# echo -e " $(cat myOutput-01.csv | grep -i zms |head -n1 | tail -n1 | awk '{print substr($6, 1, length($6)-1)}') "
-victims_channel=$(cat /root/tehR4V10L1script/tmp/myOutput-01.csv | grep -i zms |head -n1 | tail -n1 | awk '{print substr($6, 1, length($6)-1)}')
-echo -e "$victims_channel"
-
-exit 0
